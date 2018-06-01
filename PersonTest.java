e43779dcd09375a07682501c9d2fc0624d353b43
harpreet1994 <39779232+harpreet1994@users.noreply.github.com>
2018-06-01 16:21:20 -0400

    Add files via upload

---
+++ PersonTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macstudent
+ */
+public class PersonTest {
+   public static void main(String args[]){
+//       Person harpreet = new Person();
+//       harpreet.name = "harpreet";
+//       harpreet.address = "Toronto";
+//       harpreet.age = 24;
+//       System.out.println("name :" +harpreet.name + "\n addres :" + harpreet.address + "\n age :" + harpreet.age);
+//       Person sarab= new Person();
+//       sarab.setName();
+//       String nm = sarab.getName();
+//       sarab.setaddress();
+//       String add = sarab.getAddress();
+//       sarab.setAge();
+//       int age = sarab.getAge();
+//       System.out.println("name :" + nm + "\nAddress :" +add + "\n age :" + age);
+//       
+//              
+//       
+//       
+//       Person shehnaz = new Person();
+//       shehnaz.setData();
+//       System.out.println(shehnaz.toString());
+//       Person aman = new Person();
+//       System.out.print(aman.toString());
+       
+       
+       Person rimple = new Person();
+       System.out.print(rimple.toString());
+       Person rimple2 = new Person(rimple);
+       System.out.println(rimple2.toString());
+       
+   }
+       
+   } 
+
