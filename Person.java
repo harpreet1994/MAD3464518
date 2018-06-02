157b1e251254f317248ee05d8b9b7555f8902960
harpreet1994 <39779232+harpreet1994@users.noreply.github.com>
2018-06-02 17:02:45 -0400

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,16 +1,5 @@
 
-import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author macstudent
- */
+ import java.util.Scanner;
 public class Person {
    String name;
    String address;
@@ -69,3 +58,4 @@ void setAge(){
      return data;
  }
 }
+
